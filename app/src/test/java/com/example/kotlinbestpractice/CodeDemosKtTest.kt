fd472f1aa7106fdd4da59caa50dfe243355275c8
apps.ranganathan <apps.ranganathan@gmail.com>
2024-04-27 17:31:51 +0530

    countryCodeNull

--- app/src/test/java/com/example/kotlinbestpractice/CodeDemosKtTest.kt
+++ app/src/test/java/com/example/kotlinbestpractice/CodeDemosKtTest.kt
@@ -1,6 +1,7 @@
 package com.example.kotlinbestpractice
 
 import org.junit.Assert.assertEquals
+import org.junit.Assert.assertNull
 import org.junit.Assert.assertTrue
 import org.junit.Test
 
@@ -12,6 +13,9 @@ class CodeDemosKtTest {
         assertEquals("IN",getCountryCode("India"))
         val countryCodeUS = getCountryCode("United States")
         assertEquals("US", countryCodeUS)
+
+        val countryCodeNull = getCountryCode("Japan")
+        assertNull(countryCodeNull)
     }
 
 }
